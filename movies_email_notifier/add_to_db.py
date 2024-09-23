dd2e458f6a5062991b1dd4383743af52a3103f3b
Salman Ahmad <salmanahmad111499@gmail.com>
2024-09-23 14:02:02 +0500

    added .gitignore and movies_email_notified

---
+++ movies_email_notifier/add_to_db.py
@@ -0,0 +1,58 @@
+
+# this file will write existing movies names directory names to the Database
+# before writing we need to split the name in a better readable format
+# e.g Torbaaz (2024) (2160p) (YTS.MX) should be stored as Torbaaz
+
+
+# importing some important files
+import sqlite3
+import os
+
+
+# GLOBAL Variables
+DB_NAME = "movies.db"
+CURR_DIR = os.getcwd()
+WORKING_DIR = "Z:\\movies"
+
+
+# making connection to the Database
+print(f"Trying connecting to the database {DB_NAME}")
+conn = sqlite3.connect(DB_NAME)
+
+# if connection is successful
+if conn:
+    print("Connection successful")
+    # CURSOR
+    cursor = conn.cursor()
+    # Create Table Query
+    create_table_query = """
+    CREATE TABLE "movies" (
+	"name"	TEXT NOT NULL UNIQUE,
+	"id"	INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY("id")
+    );"""
+
+    # creting table (movies) in the db
+    try:
+        cursor.execute(create_table_query)
+    except sqlite3.OperationalError as e:
+        print(e)
+        exit(0)
+    
+    # after table creating, we need to iterate on root
+    # then, write all folder names in a given format to the db
+    for root, folders, files in os.walk(WORKING_DIR):
+        for index, folder in enumerate(folders):
+            altered_name = folder.split(' (')[0]
+            # adding altered name to the DB
+            try:
+                cursor.execute('''INSERT INTO movies(name) VALUES(?)
+                               ''',[altered_name])
+            except sqlite3.IntegrityError as e:
+                print(e)
+                exit(0)
+        
+    # commiting changes to the DB
+    conn.commit()
+else:
+    print(f"Connection to the databse {DB_NAME} failed.")
---
+++ movies_email_notifier/main.py
@@ -0,0 +1,129 @@
+
+
+# The script will check the files/folders within a ROOT DIRECTORY with a DB
+# if there is any folder that is present in the root but not in the DB
+# it will add it to the DB
+# then, it will send an email to the receipents(defined in a seperate Excel file) regarding what folders are added to the ROOT
+
+
+# importing some important libraries
+import os
+import sqlite3
+import ssl
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import pandas as pd
+import numpy
+from path import Path
+from dotenv import load_dotenv
+
+
+# loading our ENVIORMENTAL VARIABLES into our scope
+load_dotenv()
+
+# GLOBAL
+WORKING_DIR: str = "E:\\movies\\watched"                # root directory to walk on
+DB_NAME: str = "movies.db"                                  # name of the DB to check with
+MOVIES: list                                                # placeholder to store fetched movie names
+NEW_MOVIES_ADDED: list = []                                 # placeholder to store new movie names that are uploaded
+EXCEL_FILE = 'info.xlsx'
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+EMAIL_USERNAME = os.getenv('EMAIL_USERNAME')
+
+
+# CONFIGURATIONS
+
+# DB_Fetch_Names
+def DB_Fetch_Names() -> list:
+    # making a connection to the DB
+    try:
+        conn = sqlite3.connect(DB_NAME)
+    except:
+        # send an email to the server admin regarding failure to connect to the movies DB
+        exit(0)
+    cursor = conn.cursor()
+    cursor.execute('''SELECT name from movies''')
+    movies = cursor.fetchall()
+    conn.close()
+    return [m[0] for m in movies]
+MOVIES = DB_Fetch_Names()
+
+# add_to_DB
+def add_to_DB(name: str) -> None:
+    # connecting to the DB
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+    cursor.execute('''INSERT INTO movies(name) VALUES(?)''',[name])
+    conn.commit()
+    conn.close()
+
+# send email
+def send_email(first_name, last_name, email) -> None:
+    """
+    Sends an email to the recipient of the form.
+    :param first_name:  First name of the receiver
+    :param last_name:   Last name of the receiver
+    :param email:       Email of the receiver
+    :return:            None
+    """
+
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = EMAIL_USERNAME
+    password = EMAIL_PASSWORD
+
+    receiver = email
+    context = ssl.create_default_context()
+
+    # message string
+    message = (f"""{first_name} {last_name} new movies have been uploaded to the server.
+List of movies uploaded...
+""")
+    
+    # creating list of movies that were found
+    list_of_movies = ""
+    for index, movie in enumerate(NEW_MOVIES_ADDED):
+        list_of_movies = list_of_movies + f'{str((index + 1))}: {movie}\n'
+    
+    # appending list of movies to the mmessage
+    message = message + list_of_movies
+
+    msg = MIMEMultipart()
+    msg['From'] = username
+    msg['To'] = receiver
+    msg['Subject'] = 'Salman Server, ZED'
+
+    msg.attach(MIMEText(message))
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, msg.as_string())
+
+def iterate_and_send_email():
+    # read excel file
+    df = pd.read_excel(EXCEL_FILE)
+    # iterating on df
+    for index, row in df.iterrows():
+        send_email(row['First Name'], row['Last Name'], row['Email'])
+
+# MAIN
+# iterating on root directory
+for root, folders, files in os.walk(WORKING_DIR):
+    for folder in folders:
+        # check if there are new folders 
+        altered_name = folder.split(' (')[0]
+        # if name is not in the fetched MOVIES list
+        if altered_name not in MOVIES:
+            # add the name to the DB
+            add_to_DB(altered_name)
+            NEW_MOVIES_ADDED.append(altered_name)
+
+# we have a list of newly added movies
+# need to send an email to the receipents regarding the name of the new movies
+# if there are new directories found that are not present on the server already
+if len(NEW_MOVIES_ADDED) > 0:
+    iterate_and_send_email()
+else:
+    print("No new movies found.")
